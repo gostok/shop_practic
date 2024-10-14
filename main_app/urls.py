eca88c58ab37e988006eabb6904e33b761444490
gostok <stiv4ik12012@gmail.com>
2024-10-14 16:42:43 +0300

    The main_app is ready, pages index and about.

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
+app_name = "main"
+
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {"title": "Home", "content": "Главная страница магазина - HOME"}
+    context = {
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
+    }
     return render(request, "index.html", context)
 
 
 def about(request):
-    return render(request, "index.html")
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Текст о нас и о магазине.",
+    }
+    return render(request, "about.html", context)
