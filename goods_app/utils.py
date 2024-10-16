6e7b6bf915e32e98c85c06be4dd467c2d7f6db67
gostok <stiv4ik12012@gmail.com>
2024-10-16 14:41:35 +0300

    PostgreSQL data base connected & full text search

--- goods_app/utils.py
+++ goods_app/utils.py
@@ -1,16 +1,22 @@
+from re import search
 from .models import Products
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    # или
+    # для sqlite3
+    # keywords = [word for word in query.split() if len(word) > 2]
+    # q_objects = Q()
 
-    keywords = [word for word in query.split() if len(word) > 2]
-    q_objects = Q()
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-
-    return Products.objects.filter(q_objects)
+    # return Products.objects.filter(q_objects)
--- shop_practic/settings.py
+++ shop_practic/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     "debug_toolbar",
     "main_app",
     "goods_app",
@@ -86,8 +87,12 @@ WSGI_APPLICATION = "shop_practic.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "home",
+        "USER": "home",
+        "PASSWORD": "goStok[@o",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
